faa516aa43986a5bad64803c7878cee1e625b327
alex_home <alexzh@impulse.plus>
2020-05-29 19:19:56 +1000

    first commit

---
+++ page_main.php
@@ -0,0 +1,47 @@
+<?php
+/**
+* Template Name: Main Page Template
+*
+* @package Vkonnektu
+ */
+
+get_header();
+?>
+
+
+<div class="first-section">
+    <div class="slides">
+        <div class="slide slide-active">
+            <img src="/wp-content/themes/vkonnektu/img/house.jpg" alt="">
+            <img id="door" src="/wp-content/themes/vkonnektu/img/door_sensor.svg" alt="">
+            <img id="lights" src="/wp-content/themes/vkonnektu/img/lights.svg" alt="">
+            <img id="webcam" src="/wp-content/themes/vkonnektu/img/webcam.svg" alt="">
+            <img id="relay" src="/wp-content/themes/vkonnektu/img/single_relay.svg" alt="">
+            <div class="first-slide-text">
+                <h1>Умный дом с VKU</h1>
+                <p>позволит с комфортом управлять домашней техникой, создавать уют и обеспечивать безопасность своих родных и близких</p>
+            </div>
+        </div>
+        <div class="slide">
+            <img src="/wp-content/themes/vkonnektu/img/kitchen.jpg" alt="">
+        </div>
+        <div class="slide">
+            <img src="/wp-content/themes/vkonnektu/img/plant.jpg" alt="">
+        </div>
+    </div>
+    <div class="slider-pagination">
+        <span class="bullet bullet-active"></span>
+        <span class="bullet"></span>
+        <span class="bullet"></span>
+    </div>
+</div>
+
+<div class="second-section">
+    Wasd <br> wasd <br> <p>wasd</p> 
+</div>
+
+
+<script src="/wp-content/themes/vkonnektu/js/popper.min.js"></script>
+<script src="/wp-content/themes/vkonnektu/js/tippy.min.js"></script>
+<?php
+get_footer();
