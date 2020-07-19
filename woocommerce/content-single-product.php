1d4a42d093ea9ff3faf1f907065de8d0486a776e
alex_home <alexzh@impulse.plus>
2020-07-20 09:57:20 +1000

    same category product slider, checkout redesign

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -33,7 +33,7 @@ if ( post_password_required() ) {
 ?>
 <div id="product-<?php the_ID(); ?>" <?php wc_product_class( '', $product ); ?>>
 
-	<?php
+    <?php
 	/**
 	 * Hook: woocommerce_before_single_product_summary.
 	 *
@@ -43,8 +43,8 @@ if ( post_password_required() ) {
 	do_action( 'woocommerce_before_single_product_summary' );
 	?>
 
-	<div class="uk-width-1-3@m uk-width-1-2@s">
-		<?php
+    <div class="uk-width-1-3@m uk-width-1-2@s">
+        <?php
 		/**
 		 * Hook: woocommerce_single_product_summary.
 		 *
@@ -59,12 +59,10 @@ if ( post_password_required() ) {
 		 */
 		do_action( 'woocommerce_single_product_summary' );
 		?>
-	</div>
-
-<!-- Grid end -->
+    </div>
+    <!-- Grid end -->
 </div>
-
-	<?php
+<?php
 	/**
 	 * Hook: woocommerce_after_single_product_summary.
 	 *
@@ -76,4 +74,67 @@ if ( post_password_required() ) {
 	?>
 </div>
 
-<?php do_action( 'woocommerce_after_single_product' ); ?>
+<?php do_action( 'woocommerce_after_single_product' );?>
+<h2 class="cat-title">Товары категории <?php echo mb_strtolower( wc_get_product_category_list($product->get_id())) ?></h2>
+<div class="category-slider">
+    <div class="swiper-prod-container">
+        <div class="swiper-wrapper">
+            <?php
+				$prod_cat = array_shift(array_values($product->get_category_ids()));
+				$prod_id = get_the_ID();
+				$args = array(
+					'post_type' => 'product',
+					'posts_per_page' => 4,
+					'tax_query' => array( array(
+						'taxonomy'  => 'product_cat',
+						'field'     => 'id', 
+						'terms'     => $prod_cat
+					)), 
+					'post__not_in' => [$prod_id]);
+				$loop = new WP_Query( $args );
+				while ( $loop->have_posts() ) : $loop->the_post(); global $product; ?>
+
+            <div class="swiper-slide">
+                <a href="<?php echo get_permalink( $loop->post->ID ) ?>"
+                    title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+
+                    <?php woocommerce_show_product_sale_flash( $post, $product ); ?>
+					<div class="cat-img"> 
+						<?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog'); 
+							else echo '<img src="'.woocommerce_placeholder_img_src().'" alt="Placeholder" width="300px" height="300px" />'; ?>
+					</div>
+                    <h4><?php the_title(); ?></h4>
+
+                    <span class="price"><?php echo $product->get_price_html(); ?></span>
+
+				</a>
+			</div>
+
+            <?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+        </div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-button-next"></div>
+    </div>
+</div>
+
+<script src="/wp-content/themes/vkonnektu/js/swiper.min.js"></script>
+<script>
+let mySwiper = new Swiper('.swiper-prod-container', {
+	slidesPerView: 1,
+	spaceBetween: 70,
+	breakpoints: {
+    500: {
+      slidesPerView: 2,
+	  spaceBetween: 20,
+    },
+	900: {
+      slidesPerView: 3
+    }
+  },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
+</script>
